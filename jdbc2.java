e795848df3d27e4c702cfdd04d65ba6399718d8c
Dhiraj Jadhav <144583498+DhirajJadhav5775@users.noreply.github.com>
2024-07-27 09:47:25 +0530

    Add files via upload

---
+++ jdbc2.java
@@ -0,0 +1,36 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class jdbc2
+{
+
+    public static void JDBCconnection(String url, String username, String password)
+    {
+        try
+        {
+            String query = "insert into jdbc(Email,UserName,Passwordd)values('dhiraj@gmail.com','dhiraj123','df2e34')";
+            Connection conn = DriverManager.getConnection(url,username,password);
+            Statement statement = conn.createStatement();
+
+            statement.executeUpdate(query);
+            
+            statement.close();
+            conn.close();
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+    public static void main(String args[])
+    {
+        System.out.println("JDBC Program");
+        String url = "jdbc:mysql://localhost:3306/practise";
+        String username = "root";
+        String password = "Dhiraj@5775";
+
+        JDBCconnection(url,username,password);
+    }
+}
