ffd393ab97f3567623494b4e53f09a50d01e2ec8
Dhiraj Jadhav <144583498+DhirajJadhav5775@users.noreply.github.com>
2024-09-12 15:48:11 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,7 +2,7 @@ public class InsertionSort
 {
     public static void main(String args[])
     {
-        int arr[] = {5,6,2,3,1};
+        int arr[] = {5,6,2,3,1,70,92,4};
         System.out.println("Given Array: ");
         for(int i : arr)
         {
@@ -15,22 +15,21 @@ public class InsertionSort
 
     public static void InsertionSort(int arr[])
     {
-        int key = 0;
         for(int i = 1; i < arr.length; i++)
         {
-            key = arr[i];
+            int iTemp = arr[i];
             int j = i - 1;
-            while(j >= 0 && arr[j] > key)
+            while(j >= 0 && arr[j] > iTemp)
             {
                 arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j + 1] = key;
+            arr[j+1] = iTemp;
         }
-        System.out.println("Array After Sorting: ");
-        for(int i : arr)
+        System.out.println("Sorted Array: ");
+        for(int num : arr)
         {
-            System.out.print(" " + i);
+            System.out.print(" " + num);
         }
     }
 }
