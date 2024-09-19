077d6366cffd5311bd079d7ab882d62b6b8565d9
Dhiraj Jadhav <144583498+DhirajJadhav5775@users.noreply.github.com>
2024-09-19 16:50:32 +0530

    Add files via upload

---
+++ Collectionsframework.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Collectionsframework 
+{
+    public static void main(String args[])
+    {
+        LinkedList<Integer> llist = new LinkedList<Integer>();
+        llist.add(10);
+        llist.add(20);
+        llist.add(30);
+
+        /*for(int iCnt : llist)
+        {
+            int iret = iterator.next();
+            if(llist[iCnt] == 20)
+            {
+                iterator.remove();
+            }
+        }*/
+
+    }    
+}
