88dc4d72d60027b73bb29bffb5362a4c04b4692d
Dhiraj Jadhav <144583498+DhirajJadhav5775@users.noreply.github.com>
2024-10-09 19:45:26 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,11 @@
+let a = 15;
+let b = 25;
+let c = 10;
+
+if (a > b && a > c) {
+  console.log(a + " is the largest number.");
+} else if (b > a && b > c) {
+  console.log(b + " is the largest number.");
+} else {
+  console.log(c + " is the largest number.");
+}
