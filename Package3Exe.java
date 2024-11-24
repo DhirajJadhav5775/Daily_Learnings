7491e35e348bbed26198d491f8cbf9bc690c938a
Dhiraj Jadhav <144583498+DhirajJadhav5775@users.noreply.github.com>
2024-11-24 22:31:49 +0530

    Add files via upload

---
+++ Package3Exe.java
@@ -0,0 +1,12 @@
+import Package3.*;
+
+public class Package3Exe
+{
+    public static void main(String args[])
+    {
+        int iNo1 = 12;
+        int iNo2 = 21;
+        int iRes = Package3.Add(iNo1, iNo2);
+        System.out.println("Result Is: " + iRes);
+    }
+}
